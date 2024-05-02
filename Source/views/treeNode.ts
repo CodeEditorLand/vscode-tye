905a3f536418960c7c10eaef4a59047e75a9d94f
Nikola R. Hristov <nikola@playform.cloud>
2024-05-02 23:32:10 +0300

    squash!

--- Source/views/treeNode.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-import * as vscode from 'vscode';
-
-export default interface TreeNode {
-    getChildren?: () => TreeNode[] | Promise<TreeNode[]>;   
-    getTreeItem(): vscode.TreeItem | Promise<vscode.TreeItem>;
-}
