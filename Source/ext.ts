43beecc7c2331a5539fe36396bd9e38d3326a910
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-12 02:03:44 +0300

    squash!

--- Source/ext.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-import { UIExtensionVariables } from '@microsoft/vscode-azext-utils';
-
-const ext = {} as UIExtensionVariables;
-
-export default ext;
